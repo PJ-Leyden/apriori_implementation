5a6cf514d1a9ef1428cb1b8ea658155d374efb4a
PJ-Leyden <pleyden@kent.edu>
2019-10-04 12:15:02 -0400

    Complete apriori implementation

--- myapriori.cpp
+++ myapriori.cpp
@@ -18,6 +18,7 @@ Notes:
 #include <string>
 #include <deque>
 #include <vector>
+#include <map>
 #include <list>
 #include <algorithm>
 #include <cmath>
@@ -26,17 +27,15 @@ using std::deque; using std::vector; using std::string;
 
 //Function Prototypes
 //========================================================================
-void read_file(char*, deque<vector<bool>>&, deque<vector<int>>&);
+void read_file(char*, deque<vector<int>>&);
 void print_int_set(const deque<vector<int>>&);
 void print_bool_set(const deque<vector<bool>>&);
-deque<vector<int>> apriori_alg(const deque<vector<bool>>&, double);
+deque<vector<int>> generate_next_candidate_set(const deque<vector<int>>&, const deque<vector<int>>&);
+deque<vector<int>> generate_c1_set(const deque<vector<int>>&);
+deque<vector<int>> get_frequent_sets(const deque<vector<int>>&, const deque<vector<int>>&, double);
+deque<vector<int>> apriori_alg(const deque<vector<int>>&, double);
 //========================================================================
 
-/*
-struct item_set{
-	std::vector set;
-};
-*/
 
 //main function
 //========================================================================
@@ -49,6 +48,7 @@ int main(int argc, char** argv){
 	}
 
 	//Get Min Support
+	std::cout << "Getting Minimum Support\n";
 	int len = 0;
 	double min_support = 0;
 	for(int i = 0; argv[2][i] != '\0'; ++i){
@@ -62,15 +62,18 @@ int main(int argc, char** argv){
 	std::cout << "Min Support: " << min_support << '\n';
 
 	//Create Data Sets
-	deque<vector<bool>> bool_item_sets;
+	std::cout << "Reading Data Sets\n";
 	deque<vector<int>> int_item_sets;
-	read_file(argv[1], bool_item_sets, int_item_sets);
+	read_file(argv[1], int_item_sets);
 
 	//Calculate frequent item sets
-	deque<vector<int>> frequent_sets = apriori_alg(bool_item_sets, min_support);
+	std::cout << "Running Apriori Algorithm\n";
+	deque<vector<int>> frequent_sets = apriori_alg(int_item_sets, min_support);
+	std::cout << "Completed Apriori Algorithm\n";
 
 	//print results
-	std::cout << "Frequent Item Sets\n";
+	std::cout << "Printing Results\n";
+	std::cout << "Frequent Item Sets:\n";
 	std::cout << "=======================================\n";
 	print_int_set(frequent_sets);
 	std::cout << "=======================================\n";
@@ -82,10 +85,10 @@ int main(int argc, char** argv){
 
 //Function Implementations
 //========================================================================
-void read_file(char* file_name, deque<vector<bool>>& bool_item_sets, deque<vector<int>>& int_item_sets){
+void read_file(char* file_name, deque<vector<int>>& int_item_sets){
 	std::ifstream in_file(file_name);
 
-	//Category 1 
+	//Candidate Set 1 
 	std::vector<int> c1;
 	
 	//init int item sets
@@ -119,17 +122,16 @@ void read_file(char* file_name, deque<vector<bool>>& bool_item_sets, deque<vecto
 		}
 	}
 
-	//
-	bool_item_sets.push_back(vector<bool>(c1.size(), false));
-	for(const std::vector<int> v : int_item_sets){
-		for(int item : v){
-			(*(bool_item_sets.end() - 1))[item - 1] = true;
-		}
+	//boolean set creation code
+	/*
 		bool_item_sets.push_back(vector<bool>(c1.size(), false));
-	}
-
-
-	
+		for(const std::vector<int> v : int_item_sets){
+			for(int item : v){
+				(*(bool_item_sets.end() - 1))[item - 1] = true;
+			}
+			bool_item_sets.push_back(vector<bool>(c1.size(), false));	
+		}
+	*/
 
 	in_file.close();
 }
@@ -175,14 +177,118 @@ void print_bool_set(const deque<vector<bool>>& item_sets){
 	}
 }
 
-deque<vector<int>> apriori_alg(const deque<vector<bool>>& item_sets, double frequency){
-	/*
-	while(more candiate sets or frequent sets){
-		generate next candidate set with set length k+1
-		check candidates against item sets
-		add the frequent to a running set of frequent sets...
-		repeat with latests sets
+deque<vector<int>> generate_next_candidate_set(const deque<vector<int>>& prev_freq_sets, const deque<vector<int>>& first_freq_set){
+	deque<vector<int>> result;
+	vector<int> cur_set;
+
+	for(const vector<int>& v : prev_freq_sets){
+		for(vector<int> ele : first_freq_set){
+			if(std::find(v.begin(), v.end(), ele[0]) == v.end()){
+				//std::cerr << "Didn't find ele[0]\n";
+				vector<int> clone(v);
+				//std::cerr << "CLONE:\n";
+				//for(int x : clone){std::cerr << x;}
+				//std::cerr << "\n";
+
+				for(vector<int>::iterator itr = clone.begin(); itr != clone.end(); ++itr){
+					std::cerr << "ELE[0]: " << ele[0] << '\n';
+					std::cerr << "*itr: " << *itr << '\n';
+					if(ele[0] < *itr || itr == clone.end() - 1){
+						if(itr == clone.end() - 1 && !(ele[0] < *itr)){
+							clone.push_back(ele[0]);
+						}else{
+							clone.insert(itr, ele[0]);
+						}
+
+						if(std::find(result.begin(), result.end(), clone) == result.end()){
+							result.push_back(clone);
+						}
+						break;
+					}
+				}
+			}
+		}
 	}
-	*/
+
+	return result;
+}
+
+deque<vector<int>> generate_c1_set(const deque<vector<int>>& item_sets){
+	deque<vector<int>> c1;
+	for(const vector<int>& v : item_sets){
+		for(int x : v){
+			vector<int> temp; temp.push_back(x);
+			if(std::find(c1.begin(), c1.end(), temp) == c1.end()){
+				c1.push_back(temp);
+			}
+		}
+	}
+
+	return c1;
+}
+
+deque<vector<int>> get_frequent_sets(const deque<vector<int>>& item_sets, const deque<vector<int>>& candidate_set, double min_support){
+	deque<vector<int>> frequent_set;
+
+	std::map<vector<int>, int> frequency_map;
+	for(vector<int> v : candidate_set){
+		frequency_map.emplace(v, 0);
+	}
+
+	for(const vector<int>& c_set : candidate_set){
+		for(const vector<int>& set : item_sets){
+			vector<int>::const_iterator candidate_itr = c_set.begin();
+			vector<int>::const_iterator set_itr = set.begin();
+			while(candidate_itr != c_set.end() && set_itr != set.end()){
+				if(*candidate_itr == *set_itr){
+					++candidate_itr;
+				}
+				++set_itr;
+			}
+			if(candidate_itr == c_set.end()){
+				frequency_map.find(c_set)->second += 1;
+			}
+		}
+	}
+
+	for(auto pair : frequency_map){ 
+		double frequency = ((double)pair.second / item_sets.size()) * 100;
+		if(frequency >= min_support){
+			frequent_set.push_back(pair.first);
+		}
+	}
+
+	return frequent_set;
+}
+
+deque<vector<int>> apriori_alg(const deque<vector<int>>& item_sets, double min_support){
+
+	deque<vector<int>> result;
+	deque<vector<int>> ck;
+	deque<vector<int>> fk;
+	deque<vector<int>> f1;
+
+	int set_len = 1;
+	int max_len;
+	
+	ck = generate_c1_set(item_sets);
+	fk = get_frequent_sets(item_sets, ck, min_support);
+	f1 = fk;
+
+	for(vector<int> v : f1){result.push_back(v);}
+	++max_len;
+
+	max_len = ck.size();
+
+	while(set_len <= max_len){
+		ck = generate_next_candidate_set(fk, f1);
+		if(ck.size() == 0){break;}
+		fk = get_frequent_sets(item_sets, ck, min_support);
+		if(fk.size() == 0){break;}
+		for(vector<int> v : fk){result.push_back(v);}
+		++max_len;
+	}
+
+	return result;
 }
 //========================================================================
